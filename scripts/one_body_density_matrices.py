f57ea0e73f2bb98de4457eff2401a827beda1601
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-08-29 18:27:15 +0200

    Add working expression for rho

--- scripts/one_body_density_matrices.py
+++ scripts/one_body_density_matrices.py
@@ -6,10 +6,15 @@ from sympy.physics.secondquant import (
     NO,
     evaluate_deltas,
     substitute_dummies,
+    Commutator,
 )
-from sympy import symbols, Dummy, latex, Rational
+from sympy import symbols, Dummy, latex, Rational, Mul
 
-pretty_dummies_dict = {"above": "cdefgh", "below": "klmno", "general": "pqrstu"}
+pretty_dummies_dict = {
+    "above": "abcdef",
+    "below": "ijklmn",
+    "general": "pqrstu",
+}
 
 wicks_kwargs = {
     "simplify_dummies": True,
@@ -34,7 +39,7 @@ def get_ccsd_t_operators():
     c_abij = NO(Fd(a) * Fd(b) * F(j) * F(i))
 
     T_1 = t_ai * c_ai
-    T_2 = t_abij * c_abij
+    T_2 = Rational(1, 4) * t_abij * c_abij
 
     return (T_1, T_2)
 
@@ -49,11 +54,11 @@ def get_ccsd_lambda_operators():
     i, j = symbols("i, j", below_fermi=True, cls=Dummy)
     a, b = symbols("a, b", above_fermi=True, cls=Dummy)
 
-    l_ijab = AntiSymmetricTensor("t", (i, j), (a, b))
+    l_ijab = AntiSymmetricTensor("l", (i, j), (a, b))
     c_ijab = NO(Fd(i) * Fd(j) * F(b) * F(a))
 
     L_1 = l_ia * c_ia
-    L_2 = l_ijab * c_ijab
+    L_2 = Rational(1, 4) * l_ijab * c_ijab
 
     return (L_1, L_2)
 
@@ -64,16 +69,37 @@ c_pq = Fd(p) * F(q)
 T = sum(get_ccsd_t_operators())
 L = sum(get_ccsd_lambda_operators())
 
-# Only keep non-zero terms
-rho_eq = (1 - T) * c_pq * (1 + T)
-rho_eq += (
-    L
-    * (1 - T + Rational(1, 2) * T ** 2)
-    * c_pq
-    * (1 + T + Rational(1, 2) * T ** 2)
-)
 
-rho = wicks(rho_eq, **wicks_kwargs).expand()
+def eval_equation(eq):
+    eq = wicks(eq, **wicks_kwargs)
+    eq = evaluate_deltas(eq.expand())
+    eq = substitute_dummies(eq, **sub_kwargs)
+
+    return eq
+
+
+# Only keep non-zero terms
+
+rho_eq = eval_equation(c_pq)
+rho_eq += eval_equation(Commutator(c_pq, T))
+rho_eq += eval_equation(L * c_pq)
+comm = Commutator(c_pq, T)
+rho_eq += eval_equation(L * comm)
+comm = Commutator(comm, sum(get_ccsd_t_operators()))
+rho_eq += Rational(1, 2) * eval_equation(L * comm)
+# rho_eq = wicks((1 - T) * c_pq * (1 + T), **wicks_kwargs)
+# rho_eq += wicks(
+#    L
+#    * (1 - T + Rational(1, 2) * T * T)
+#    * c_pq
+#    * (1 + T + Rational(1, 2) * T * T),
+#    **wicks_kwargs
+# )
+
+rho = rho_eq.expand()
+# rho = wicks(rho_eq, **wicks_kwargs)
+for term in rho.args:
+    print(evaluate_deltas(term))
 rho = evaluate_deltas(rho)
 rho = substitute_dummies(rho, **sub_kwargs)
 
